c1c53c2d3f637166daa2c305e3d656a9eadf2386
manuamv92 <manuamv@outlook.es>
2022-06-08 12:59:27 +0200

    adding the first arrays

---
+++ RLG.js
@@ -0,0 +1,5 @@
+//those are the arrays who contain the pieces of the Village//
+
+const vSize = ['Outpost', 'Hamlet', 'Village'];
+const vAge = ['something between 300 and 1000 years old', 'something between 100 and 300 years old', 'something between 50 and 100 years old', 'freshly built'];
+const vRulerOddity = ['cruel'];
